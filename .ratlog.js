e0cf1b79ed5b6935ed776b8cd25c08c38b4b1ced
cnuebred <jonatann01ew@gmail.com>
2022-02-24 23:57:40 +0100

    update sample config in ratlog.js

--- .ratlog.js
+++ .ratlog.js
@@ -1,15 +1,25 @@
 module.exports = {
     logger: {
         dir: 'logs',
-        filename: () => { return `ratlog-${new Date().getDate()}` },
+        filename: () => {
+            const time = new Date()
+            const timeHMS = [time.getDay(), time.getMonth()]
+            return `ratlog_${timeHMS.map((item) => `0${item}`.slice(-2)).join('-')}`
+        },
         extension: 'rat',
         algorithm: 'aes-256-ctr',
         secret: 'd47424114c7f3c8df4fa7205c83ff0bd'
     },
-    methods: {},
+    methods: {
+        time_endpoint: () => { return '[@{blue bold}#{time}@{normal}]' },
+        text_status: (text) => { return `@{normal}| Status: @{${text === 'ok' ? 'green' : 'red'} bold}#{:} @{normal}|` }
+    },
     renders: {
-        file: {
-            render: ['[@{blue bold underline}#{file}@{normal}]']
+        endpoint: {
+            render: [
+                '#{time_endpoint}|>',
+                '@{yellow}#{.}'
+            ]
         }
     }
 }
