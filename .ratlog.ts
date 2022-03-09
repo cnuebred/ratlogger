cdad0c5ef97fb63f1fe2e91977b69f652a7d874b
cnuebred <jonatann01ew@gmail.com>
2022-03-09 13:09:38 +0100

    change extension of file from cjs

--- .ratlog.ts
+++ .ratlog.ts
@@ -1,18 +1,19 @@
-module.exports = {
+export default {
     logger: {
         dir: 'logs',
         filename: () => {
             const time = new Date()
-            const timeHMS = [time.getDay(), time.getMonth()]
-            return `ratlog_${timeHMS.map((item) => `0${item}`.slice(-2)).join('-')}`
+            const formatDay = `0${time.getDay()}`.slice(-2)
+            const formatMonth = `0${time.getMonth()}`.slice(-2)
+            return `ratlog_${formatDay}-${formatMonth}`
         },
         extension: 'rat',
         algorithm: 'aes-256-ctr',
         secret: 'd47424114c7f3c8df4fa7205c83ff0bd'
     },
     methods: {
-        time_endpoint: () => { return '[@{blue bold}#{time}@{normal}]' },
-        text_status: (text) => { return `@{normal}| Status: @{${text === 'ok' ? 'green' : 'red'} bold}#{:} @{normal}|` }
+        time_endpoint: () => '[@{blue bold}#{time}@{normal}]',
+        text_status: (text) => `@{normal}| Status: @{${text === 'ok' ? 'green' : 'red'} bold}#{:} @{normal}|`
     },
     renders: {
         endpoint: {
